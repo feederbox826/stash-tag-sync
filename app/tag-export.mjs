bfffda045031e580a26b83fbd6057102c064bd1b
feederbox826 <feederbox826@users.noreply.github.com>
2025-11-29 21:18:50 -0500

    remove downloading

--- app/tag-export.mjs
+++ app/tag-export.mjs
@@ -1,27 +1,23 @@
 import axios from "axios";
 import fs from "fs/promises";
 import * as https from "https";
-import mime from 'mime/lite';
 import cliProgress from "cli-progress";
 import rs from "route-serve";
 import cron from "node-cron";
 import { imageSizeFromFile } from "image-size/fromFile";
-import { checksumFile } from "./md5.mjs";
 import 'dotenv/config';
 
 // config
 const APIKEY = process.env.STASH_APIKEY;
 const STASH_URL = process.env.STASH_URL;
-const STASHDB_APIKEY = process.env.STASHDB_APIKEY;
 const TAG_PATH = process.env.TAG_PATH || "./tags";
 const CACHE_PATH = process.env.CACHE_PATH || "./cache";
 const IMG_FILETYPES = ["jpg", "png", "webp", "svg"];
 const VID_FILETYPES = ["mp4", "webm"];
 const TAG_EXPORT_PATH = process.env.TAG_EXPORT_PATH || `${CACHE_PATH}/tags-export.json`;
 const EXCLUDE_PREFIX = ["r:", "c:", ".", "stashdb", "Figure", "["]
-const RECHECK_ETAG = process.env.RECHECK_ETAG || false;
-const ETAG_FILE_PATH = `${CACHE_PATH}/etags.json`;
-const STASHID_FILE_PATH = `${CACHE_PATH}/stashid.json`;
+
+const STASHDB_URL = "https://stashdb.org/graphql"
 
 // setup axios agent without TLS verification
 const stashAgent = axios.create({
@@ -31,54 +27,22 @@ const stashAgent = axios.create({
   })
 })
 
-const stashdbAgent = axios.create({
-  headers: { 'ApiKey': STASHDB_APIKEY },
-})
-
-async function getTagStashID(name) {
-  const query = `query ($name: String!) {
-    findTagOrAlias(name: $name) { id deleted }}`
-  const variables = { name };
-  return stashdbAgent.post(
-    "https://stashdb.org/graphql",
-    { query, variables }
-  ).then(res => res.data.data.findTagOrAlias)
-    .then(data => data.deleted ? null : data.id)
-    .catch(err => err.response);
-}
-
-const parseFile = (filepath) =>
-  fs.access(filepath)
-    .then(async () => JSON.parse(await fs.readFile(filepath)))
-    .catch(() => {});
-
 // get all performers
 async function getAllTags() {
   const query = `query {
     findTags(filter: { per_page: -1 }) {
     tags {
       name aliases image_path id ignore_auto_tag
+      stash_ids {
+        stash_id
+        endpoint
+      }
     }}}`;
   const response = await stashAgent.post(
     STASH_URL,
     { query }
   ).catch(err => err.response);
   return response.data.data.findTags.tags;
-}
-
-async function downloadFile(url, etagMap, force = false) {
-  const etag = etagMap.get(url);
-  const response = await stashAgent.get(url, {
-    method: "GET",
-    responseType: "arraybuffer",
-    responseEncoding: "binary",
-    headers: {
-      "If-None-Match": force ? "" : etag 
-    }
-  }).catch(err => err.response);
-  const etagHeader = response.headers["etag"];
-  if (etagHeader) etagMap.set(url, etagHeader);
-  return response
 }
 
 // win-1252 conversion from https://stackoverflow.com/a/73127563
@@ -131,18 +95,6 @@ const getAltFiles = async(dir) =>
       .map(f => f.split(".")[0].replace(/ \(\d\)/, ""))
     ).catch(() => []);
 
-const getCachedStashID = async(tagName, stashidMap) => {
-  const cachedID = await stashidMap.get(tagName);
-  if (cachedID) return cachedID;
-  const stashID = await getTagStashID(tagName);
-  if (stashID) {
-    stashidMap.set(tagName, stashID);
-    // write stashids map
-    fs.writeFile(STASHID_FILE_PATH, JSON.stringify(Object.fromEntries(stashidMap), null, 2));
-  }
-  return stashID;
-}
-
 // main function
 async function main() {
   console.log("Starting tag sync");
@@ -151,24 +103,14 @@ async function main() {
   }, cliProgress.Presets.shades_classic);
   // create tag inventory
   const tagInventory = {};
-  // load etags map
-  const etags = await parseFile(ETAG_FILE_PATH);
-  const etagMap = etags ? new Map(Object.entries(etags)) : new Map();
-  // load stashids map
-  const stashids = await parseFile(STASHID_FILE_PATH);
-  const stashidMap = stashids ? new Map(Object.entries(stashids)) : new Map();
   // get all tags
   const newTags = await getAllTags();
   // iterate over tags
   const length = newTags.length;
   const totalbar = multibar.create(length, 0, { name: "Total", last: "" });
   totalbar.update({ name: "Total" });
-  const dlbar = multibar.create(length, 0, { name: "Downloaded", last: "" });
-  dlbar.update({ name: "Downloaded" });
   const skipbar = multibar.create(length, 0, { name: "Skipped", last: "" });
   skipbar.update({ name: "Skipped" });
-  const stuffbar = multibar.create(length, 0, { name: "Stuffed", last: "" });
-  stuffbar.update({ name: "Stuffed" });
   const altFiles = await getAltFiles(`${TAG_PATH}/alt/`);
   // get all files
   const allFiles = await getAllFiles();
@@ -180,7 +122,7 @@ async function main() {
     // set up names
     const tagName = tag.name;
     // get stashID
-    const stashID = await getCachedStashID(tagName, stashidMap);
+    const stashID = tag?.stash_ids.find(s => s.endpoint === STASHDB_URL)?.stash_id
     const fileName = cleanFileName(tagName);
     const filePath = `${TAG_PATH}/${fileName}`;
     // if raw file exists, delete (erroneous or leftover)
@@ -205,40 +147,8 @@ async function main() {
       console.error("No stashID found for tag:", tagName);
     };
     tagInventory[tagName] = { img: imgFiles[0], vid: vidFiles[0], ignore, alt, imgDimensions: dimensions, aliases: tag.aliases, stashID };
-    // if no file, force download
-    const force = !imgFiles.length && !vidFiles.length;
-    const hasEtag = etagMap.has(url);
-    if (!force && !hasEtag) { // try forcing etag if exists
-      const forceEtag = await checksumFile(vidFiles[0] || imgFiles[0]);
-      etagMap.set(url, `"${forceEtag}"`);
-      stuffbar.increment({ last: tagName })
-    } else if (!RECHECK_ETAG && hasEtag) { // if not forcedl, skip if exists in etags
-      skipbar.increment({ last: tagName });
-      continue;
-    }
-    // download file
-    const response = await downloadFile(url, etagMap, force);
-    if (response.status == 304) {
-      skipbar.increment({ last: tagName });
-    } else if (response.status == 200) {
-      // get ext
-      const ext = mime.getExtension(response.headers["content-type"]);
-      dlbar.increment({ last: tagName });
-      multibar.log("Downloading", tagName);
-      const bufferData = Buffer.from(response.data, "binary");
-      await fs.writeFile(filePath, bufferData);
-      // rename file extension
-      // ovewrites files of existing type, leaves previous types alone
-      const extFileName = `${filePath}.${ext}`;
-      fs.rename(filePath, extFileName);
-      // push to tag inventory
-      const fileType = VID_FILETYPES.includes(ext) ? "vid" : "img";
-      tagInventory[tagName][fileType] = extFileName;
-    }
   }
   multibar.stop()
-  // write etags map
-  fs.writeFile(ETAG_FILE_PATH, JSON.stringify(Object.fromEntries(etagMap)));
   // finally, write tag inventory
   const saniExport = saniTagExports(tagInventory);
   fs.writeFile(TAG_EXPORT_PATH, JSON.stringify(saniExport));
